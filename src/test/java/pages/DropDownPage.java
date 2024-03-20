908bd45b11a8ce319b200499253ba0df1a71d9e9
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-03-20 22:35:19 +0300

    added some codes

--- src/test/java/pages/DropDownPage.java
+++ src/test/java/pages/DropDownPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.devtools.v85.page.*;
+import org.openqa.selenium.devtools.v85.webaudio.model.*;
 import org.openqa.selenium.support.*;
 import utilities.*;
 
@@ -16,7 +17,6 @@ public class DropDownPage {
     //2. Select Illinois --> select by visible text
     //3. Select Virginia --> select by value
     //4. Select California --> select by index
-    //5. Verify final selected option is California.
 
     //TC#2
     //Select “December 1st, 1950g” and verify it is selected.
@@ -29,10 +29,13 @@ public class DropDownPage {
     //1.Go to http://practice.cydeo.com/dropdown
     //2. Select all the options from multiple language select dropdown.
     //3. Print out all selected values and verify each is selected.
-    //4.Deselect all values and verify each is deselected.
+    //4. Deselect all values and verify each is deselected.
 
 
     @FindBy(id = "state")
     public WebElement stateDropDown;
+
+    @FindBy(name = "Languages")
+    public WebElement languagesDropDown;
 
 }
--- src/test/java/tests/DropDownTest.java
+++ src/test/java/tests/DropDownTest.java
@@ -1,20 +1,29 @@
 package tests;
 
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.*;
 import org.testng.*;
 import org.testng.annotations.*;
 import pages.*;
 import utilities.*;
 
+import java.util.*;
+
 public class DropDownTest {
 
     DropDownPage dropDownPage = new DropDownPage();
 
+    @BeforeMethod
+    public void goToPage() {
+        Driver.getDriver().get("http://practice.cydeo.com/dropdown");
+
+    }
+
     @Test
-    public void test1(){
+    public void test1() {
 
         //1. Go to http://practice.cydeo.com/dropdown
-        Driver.getDriver().get("http://practice.cydeo.com/dropdown");
+        //Driver.getDriver().get("http://practice.cydeo.com/dropdown");
 
         //2. Select Illinois --> select by visible text
         Select selectState = new Select(dropDownPage.stateDropDown);
@@ -31,7 +40,33 @@ public class DropDownTest {
         String acutalOption = selectState.getFirstSelectedOption().getText();  // california
         String expectedOption = "California";
         Assert.assertEquals(acutalOption, expectedOption);
+    }
 
+    @Test
+    public void test3() {
+
+
+        //1.Go to http://practice.cydeo.com/dropdown
+        //Driver.getDriver().get("http://practice.cydeo.com/dropdown");
+
+        //2. Select all the options from multiple language select dropdown.
+        //3. Print out all selected values and verify each is selected.
+
+        Select languageSelect = new Select(dropDownPage.languagesDropDown);
+        List<WebElement> options = languageSelect.getOptions();
+
+        for (WebElement option : options) {
+            languageSelect.selectByVisibleText(option.getText());
+            System.out.println(option.getText());
+            Assert.assertTrue(option.isSelected());
+        }
+
+        //4. Deselect all values and verify each is deselected.
+        languageSelect.deselectAll();
+
+        for (WebElement option : options) {
+            Assert.assertFalse(option.isSelected());
+        }
 
     }
 }
