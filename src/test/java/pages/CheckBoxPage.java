82948e13cb981debeb5bc8b595dba6b2a285da68
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-03-20 21:26:07 +0300

    added some codes

--- src/test/java/pages/CheckBoxPage.java
+++ src/test/java/pages/CheckBoxPage.java
@@ -8,4 +8,13 @@ public class CheckBoxPage {
     public CheckBoxPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    //TC #1:checkboxes
+    //1. Go to http://practice.cydeo.com/checkboxes
+    //2. Confirm checkbox #1 is NOT selected by default
+    //3. Confirm checkbox #2 is SELECTED by default.
+    //4. Click checkbox #1 to select it.
+    //5. Click checkbox #2 to deselect it.
+    //6. Confirm checkbox #1 is SELECTED.
+    //7. Confirm checkbox #2 is NOT selected.
 }
