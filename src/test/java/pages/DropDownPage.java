fcd9d895c724dce66b3e019b1d42837249b7bd2d
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-03-20 21:41:05 +0300

    added some codes

---
+++ src/test/java/pages/DropDownPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.devtools.v85.page.*;
+import org.openqa.selenium.support.*;
+import utilities.*;
+
+public class DropDownPage {
+
+    public DropDownPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    //TC#1
+    //1. Go to http://practice.cydeo.com/dropdown
+    //2. Select Illinois --> select by visible text
+    //3. Select Virginia --> select by value
+    //4. Select California --> select by index
+    //5. Verify final selected option is California.
+
+    //TC#2
+    //Select “December 1st, 1950g” and verify it is selected.
+    //1. Go to http://practice.cydeo.com/dropdown
+    //2. Select Year --> by visible text
+    //3. Select Month -->  by value
+    //4. Select Day -->  by index
+
+    //TC#3
+    //1.Go to http://practice.cydeo.com/dropdown
+    //2. Select all the options from multiple language select dropdown.
+    //3. Print out all selected values and verify each is selected.
+    //4.Deselect all values and verify each is deselected.
+
+
+}
