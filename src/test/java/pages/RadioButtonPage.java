15f8a7f49e0f6f3df5b5472d8ceeeb66d820bc1b
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-03-20 21:02:22 +0300

    added some codes

--- src/test/java/pages/RadioButtonPage.java
+++ src/test/java/pages/RadioButtonPage.java
@@ -7,6 +7,17 @@ import utilities.*;
 
 public class RadioButtonPage {
 
+    //TC #1:radio buttons
+    //1.GO TO: http://practice.cydeo.com/radio_buttons
+    //2. Confirm radio button #Blue is selected by default
+    //3. Confirm radio button #Football is NOT selected by default
+    //4. Click radio button #Football to select it.
+    //5. Verify radio button #Football is selected.
+    //6. Click radio button #Red to select it.
+    //7. Verify radio button #Red is selected.
+    //8. Verify radio button #Blue is not selected.
+    //9. Verify radio button #Green is not Enabled.
+
 
     public RadioButtonPage(){
         PageFactory.initElements(Driver.getDriver(),this);
@@ -15,4 +26,6 @@ public class RadioButtonPage {
 
     @FindBy(id = "sdfds")
     public WebElement button2;
+
+
 }
