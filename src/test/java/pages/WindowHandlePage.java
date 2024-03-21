b505c31b94333811e7a8afe9e1ecdcf1c009f2fa
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-03-21 20:16:19 +0300

    added some codes

---
+++ src/test/java/pages/WindowHandlePage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.support.*;
+import utilities.*;
+
+public class WindowHandlePage {
+
+    public WindowHandlePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    //1. Go to website: http://practice.cydeo.com/windows
+    //2. Assert: Title is “Windows”
+    //3. Click to: “Click Here” link
+    //4. Click to: “Cydeo” link
+    //5. Switch to Cydeo page
+    //6. Assert: Title is “Cydeo”
+    //7.Switch back to main handle
+
+}
